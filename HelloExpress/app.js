1e62a1a4858a4d206e6b5a4996b40a42fb149103
Alexandru Tenche <alex.tenche@gmail.com>
2014-12-03 23:12:51 +0200

    added partials and bootstrap to helloexpress

--- HelloExpress/app.js
+++ HelloExpress/app.js
@@ -1,24 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-	res.send('<h1>Hello</h1> Express');
+	res.render('default', {
+		title :'Home',
+		classname :'home',
+		users : ['Ray', 'Morten', 'James']
+	});
 });
 
-app.get('/me', function(req, res){
-	res.send('@alexTenche');
+app.get('/about', function(req, res){
+	res.render('default', {
+		title :'About Us',
+		classname :'about'
+	});
 });
 
-app.get('/who/:name?', function(req, res){
-	var name = req.params.name;
-	res.send(name +' was here');
-});
-
-app.get('/who/:name?/:title?', function(req, res){
-	var name = req.params.name;
-	var title = req.params.title;
-	res.send('<p>name: ' + name + '<br> title: ' + title + '</p>');
-});
 
 app.get('*', function(req, res){
 	res.send('Bad Route');
