7234d0e66fcc80e18239dd1c876bb7a8b768a280
Alexandru Tenche <alex.tenche@gmail.com>
2014-12-05 04:20:08 +0200

    finished preparyng to deploy to heroku

--- rouxmeet/routes/index.js
+++ rouxmeet/routes/index.js
@@ -5,38 +5,53 @@ var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res) {
 	var myArtwork = [];
+	var myArtists = [];
+
+	myArtists = appdata.speakers;
 	appdata.speakers.forEach(function(item){
 		myArtwork = myArtwork.concat(item.artwork);
 	});
   	res.render('index', { 
   		title: 'Home',
-  		artwork: myArtwork
+  		artwork: myArtwork,
+  		artists: myArtists,
+  		page: 'home'
   	});
 });
 
 /* GET speakers page. */
 router.get('/speakers', function(req, res) {
 	var myArtwork = [];
+	var myArtists = [];
+
+	myArtists = appdata.speakers;
 	appdata.speakers.forEach(function(item){
 		myArtwork = myArtwork.concat(item.artwork);
 	});
   	res.render('speakers', { 
   		title: 'Speakers',
-  		artwork: myArtwork
+  		artwork: myArtwork,
+  		artists: myArtists,
+  		page: 'artistList'
   	});
 });
 
-/* GET speakers page. */
+/* GET speakers detail page. */
 router.get('/speakers/:speakerid', function(req, res) {
 	var myArtwork = [];
+	var myArtists = [];
+
 	appdata.speakers.forEach(function(item){
 		if(item.shortname == req.params.speakerid){
+			myArtists.push(item);
 			myArtwork = myArtwork.concat(item.artwork);
 		}
 	});
   	res.render('speakers', { 
   		title: 'Speakers',
-  		artwork: myArtwork
+  		artwork: myArtwork,
+  		artists: myArtists,
+  		page: 'artistDetail'
   	});
 });
 
