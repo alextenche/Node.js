3b65ed6e6411ddb44f2f58e44feb257957e690d5
alextenche <alex.tenche@gmail.com>
2014-12-10 02:02:09 +0200

    updated recipes app - prety cool

--- Basics/recipes/script.js
+++ Basics/recipes/script.js
@@ -7,9 +7,6 @@ var recipes = require('./data/recipes').data;
 app.get('/', function(req, res){
   res.render('index.ejs', {title: 'Clever Kitchens'});
 });
-
-
-  
    
 app.get('/recipes', function(req, res){
   res.render('recipes.ejs', {
@@ -19,7 +16,18 @@ app.get('/recipes', function(req, res){
 });
 
 app.get('/recipes/:title', function(req, res) {
- res.send('<h1>' + req.params.title + '</h1>');
+	var data = recipes.filter(function(recipe){
+		return(recipe.url === req.params.title);
+	});
+
+	if(data.length > 0){
+		data = data[0];
+		data.title = "Clever Kitchens - Recipe";
+
+		res.render('recipe.ejs', data);
+	} else {
+		res.status(404).render('error.ejs', {title: 'Recipe Not Found'});
+	}
 });
 
 app.get('/*', function(req, res) {
