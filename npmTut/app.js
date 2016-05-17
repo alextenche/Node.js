3aaad93276f47293b6da14c35e4fcf3e1279ab28
Alex Tenche <alex.tenche@gmail.com>
2016-05-17 15:15:15 +0300

    added npmTut

---
+++ npmTut/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var unsecurePass = "password";
+
+var colors = require("colors");
+var bcrypt = require("bcrypt");
+bcrypt.genSalt(10, function(err, salt){
+  bcrypt.hash(unsecurePass, salt, function(err, hash){
+    console.log(hash.green);
+  });
+});
