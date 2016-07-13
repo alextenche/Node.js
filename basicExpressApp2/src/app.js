d780a1d99aea109e6bd23259e41fb7b1904b2fbe
Alex Tenche <alex.tenche@gmail.com>
2016-07-13 23:10:22 +0300

    finished basicExpress2 tutorial

--- basicExpressApp2/src/app.js
+++ basicExpressApp2/src/app.js
@@ -3,24 +3,41 @@
 var express = require("express");
 var posts = require("./mock/posts.json");
 
+var postsList = Object.keys(posts).map(function(value){
+  return posts[value];
+});
+
 var app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + "/templates");
 
 app.get("/", function(req, res){
-  res.render('index');
+  var path = req.path;
+  // res.locals.path = path;
+  res.render('index', {path: path});
 });
 
 app.get("/blog/:title?", function(req, res){
   var title = req.params.title;
   if(title === undefined){
     res.status(503);
-    res.send("Page not found");
+    res.render('blog', {posts: postsList});
   } else {
     var post = posts[title] || {};
     res.render('post', {post: post});
   }
+});
+
+app.get('/posts', function(req, res){
+  if(req.query.raw){
+    res.json(posts);
+  } else {
+    res.json(postsList);  
+  };
+
 });
 
 app.listen(3000, function(){
