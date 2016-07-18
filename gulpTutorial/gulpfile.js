cfbccab948d184a916d89c2677cf7063386e0b14
Alex Tenche <alex.tenche@gmail.com>
2016-07-19 00:19:03 +0300

    finished gulp tutorial

--- gulpTutorial/gulpfile.js
+++ gulpTutorial/gulpfile.js
@@ -6,29 +6,47 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
+var del = require('del');
 
 gulp.task('concatScripts', function(){
-  gulp.src(['js/jquery.js', 'js/sticky/jquery.sticky.js', 'js/main.js'])
+  return gulp.src(['js/jquery.js', 'js/sticky/jquery.sticky.js', 'js/main.js'])
+  .pipe(maps.init())
   .pipe(concat('app.js'))
+  .pipe(maps.write('./'))
   .pipe(gulp.dest('js'));
 });
 
-gulp.task('minifyScripts', function(){
-  gulp.src('js/app.js')
+gulp.task('minifyScripts', ['concatScripts'], function(){
+  return gulp.src('js/app.js')
   .pipe(uglify())
   .pipe(rename('app.min.js'))
   .pipe(gulp.dest('js'));
 });
 
 gulp.task('compileSass', function(){
-  gulp.src('scss/application.scss')
+  return gulp.src('scss/application.scss')
   .pipe(maps.init())
   .pipe(sass())
   .pipe(maps.write('./'))
   .pipe(gulp.dest('css'));
 });
 
+gulp.task('watchFiles', function(){
+  gulp.watch(['scss/**/*.scss'], ['compileSass']);
+  gulp.watch('js/main.js', ['concatScripts'])
+});
 
-gulp.task('default', ['hello'], function(){
-  console.log('this is the default task');
+gulp.task('clean', function(){
+  del(['dist', 'css/application.css*', 'js/app*.js*']);
+});
+
+gulp.task('build', ['minifyScripts', 'compileSass'], function(){
+  return gulp.src(['css/application.css', 'js/app.min.js', 'index.html', 'img/**', 'fonts/**'], {base: './'})
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task('default', ['clean'], function(){
+  gulp.start('build');
 });
